62756d5c8719e58eb586e91766b8459098a000ec
村越大樹 TB2 <murakosi@cse_hn_domain.cseltd.co.jp>
2018-07-26 14:41:30 +0900

    valueset test

--- app/lib/metadata/crud.rb
+++ app/lib/metadata/crud.rb
@@ -66,7 +66,7 @@ module Metadata
 			
 			value_types = [headers, types].transpose
 			#metadata = Metadata::ValueFieldSupplier.rebuild(metadata_type, value_types, metadata)
-			metadata = [{:full_name => "RB__c.a__c", :type=>"picklist", :label => "a", :ValueSettings => {:valueName => "lst1"}}]
+			metadata = [{:full_name => "RB__c.a__c", :type=>"Picklist", :label => "a", :ValueSettings => {:valueName => "lst1"}}]
 			#p metadata
 			#fake_response
 			save_result = Service::MetadataClientService.call(sforce_session).create(metadata_type, metadata)
