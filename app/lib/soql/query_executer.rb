4db279a94fc3ac123aa3f41c025854f7dacf078c
murakosi <37989181+murakosi@users.noreply.github.com>
2019-05-13 11:28:30 +0900

    Update query_executer.rb

--- app/lib/soql/query_executer.rb
+++ app/lib/soql/query_executer.rb
@@ -10,10 +10,10 @@ module Soql
                 soql.delete!(";");
             end
 
-            #query_result = Service::SoapSessionService.call(sforce_session).query(soql)
-            query_result = Service::ToolingClientService.call(sforce_session).query(soql)
+            query_result = Service::SoapSessionService.call(sforce_session).query(soql)
+            #query_result = Service::ToolingClientService.call(sforce_session).query(soql)
 
-            if query_result.empty?
+            if query_result.nil?
                raise StandardError.new("No matched records found")
             end
 
