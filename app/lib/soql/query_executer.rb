ce56f67bb3d2ad73f105fbc8429aad9c88708877
murakosi <37989181+murakosi@users.noreply.github.com>
2019-06-28 09:27:03 +0900

    Update query_executer.rb

--- app/lib/soql/query_executer.rb
+++ app/lib/soql/query_executer.rb
@@ -61,6 +61,8 @@ module Soql
                         record.merge!(parse_child(k, v))
                     else
                         if @check_keys.include?(k.to_s.upcase)
+                            p k
+                            p v
                             record.merge!({k => v})
                         else
                             record.merge!({@check_keys[field_count] => nil})
