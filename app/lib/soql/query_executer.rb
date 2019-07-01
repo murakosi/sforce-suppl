43711474cad5257ed06fa989449efceaa94e3d98
murakosi <37989181+murakosi@users.noreply.github.com>
2019-07-01 18:54:29 +0900

    Update query_executer.rb

--- app/lib/soql/query_executer.rb
+++ app/lib/soql/query_executer.rb
@@ -218,7 +218,8 @@ module Soql
             upcase_soql = soql.upcase
 
             #@check_keys.flatten!.sort! {|a, b| upcase_soql.index(a) <=> upcase_soql.index(b) }
-            p @query_fields = @query_fields.sort{|(k1, v1), (k2, v2)| upcase_soql.index(k1) <=> upcase_soql.index(k2) }
+            temp_array = @query_fields.sort{|(k1, v1), (k2, v2)| upcase_soql.index(k1) <=> upcase_soql.index(k2) }
+            p @query_fields = Hash[*temp_array.flatten(1)]
         end
 
         def genereate_query_fields(field_name, type = nil)
