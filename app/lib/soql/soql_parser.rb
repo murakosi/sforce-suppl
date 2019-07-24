924e30c81f1141e87e62deeca68075004b910994
murakosi <37989181+murakosi@users.noreply.github.com>
2019-07-24 18:58:02 +0900

    Update soql_parser.rb

--- app/lib/soql/soql_parser.rb
+++ app/lib/soql/soql_parser.rb
@@ -72,7 +72,7 @@ module Soql
     rule(:reserved){
       #exp("('a' 'b'?)").as(:rooter)
       #exp("/abc/i").as(:am)
-      match("SELECT")
+      exp("('SELECT'|'FROM')")
     }
 
     rule(:query_field_list){
