f07b1227a7256cb71e24573604132c2ff89cbbf8
murakosi <37989181+murakosi@users.noreply.github.com>
2019-09-25 15:52:27 +0900

    Update soqlexecuter.js

--- app/assets/javascripts/soqlexecuter.js
+++ app/assets/javascripts/soqlexecuter.js
@@ -888,10 +888,6 @@ const coordinates = function() {
   $("#soqlArea .tabArea").tabs(); 
   $("#soqlTabs").sortable({items: 'li:not(.add-tab-li)', delay: 150});
   createTab();
-  
-  for(let i = 0; i<200;i++){
-    $("#soqlHistory ul").append('<li>aaaaaaaaaaaaaaaaaaa</li>');
-  }
 
 };
 
